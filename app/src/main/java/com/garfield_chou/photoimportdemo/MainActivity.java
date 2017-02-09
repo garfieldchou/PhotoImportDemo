d13f43e9e7d449333be5fc198b2f535c67af8ace
garfield_chou <garfield_chou@asus.com>
2017-02-09 17:21:01 +0800

    Create an intent to pick up a photo from MediaStore

--- app/src/main/java/com/garfield_chou/photoimportdemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/photoimportdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.garfield_chou.photoimportdemo;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivity(intent);
+
     }
 }
