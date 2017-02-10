b362c6ae1d52f3e19083efc2357dae4e6fab8a2e
garfield_chou <garfield_chou@asus.com>
2017-02-10 09:47:17 +0800

    Deal with permission to READ_EXTERNAL_STORAGE
    1, check permission to READ_EXTERNAL_STORAGE
    2, If it didn't have the permission, request it

--- app/src/main/java/com/garfield_chou/photoimportdemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/photoimportdemo/MainActivity.java
@@ -1,24 +1,56 @@
 package com.garfield_chou.photoimportdemo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import java.io.IOException;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void getPhoto () {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                getPhoto();
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, 1);
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[] {android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+
+        } else {
+
+            getPhoto();
+            
+        }
 
     }
 
@@ -36,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
                 ImageView imageView = (ImageView) findViewById(R.id.imageView);
                 imageView.setImageBitmap(bitmap);
-                
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
